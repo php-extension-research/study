167a891d86d212803c7a7a0b4a7c52b2cfc96bf1
codinghuang <2812240764@qq.com>
2019-07-01 00:32:56 +0800

    update the test.php

--- test.php
+++ test.php
@@ -1,6 +1,9 @@
 <?php
 
-Study\Coroutine::create(function ($a, $b){
+function task($a, $b)
+{
 	echo $a . PHP_EOL;
 	echo $b . PHP_EOL;
-}, 'a', 'b');
+}
+
+Study\Coroutine::create('task', 'a', 'b');
