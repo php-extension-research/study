bfb9b78983c5a78950430ec17a69056ed6b557e9
codinghuang <15070967610@163.com>
2019-10-01 17:31:14 +0800

    add: add channel examples

---
+++ examples/coroutine/channel/push_pop.php
@@ -0,0 +1,19 @@
+<?php
+
+study_event_init();
+
+$chan = new Study\Coroutine\Channel();
+
+Sgo(function () use ($chan) {
+    var_dump("push start");
+    $ret = $chan->push("hello world");
+    var_dump($ret);
+});
+
+Sgo(function () use ($chan) {
+    var_dump("pop start");
+    $ret = $chan->pop();
+    var_dump($ret);
+});
+
+study_event_wait();
